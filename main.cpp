c1b5a541ad866300109b52256e29a855199e8378
Sumit mhalaskar <sumit.mhalaskar24@sanjivani.edu.in>
2024-09-01 20:56:11 +0530

    Add a to integers cpp lab

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+ using namespace std;
+
+int main()
+{
+int first_number,second_number;
+cout<<"entre a two number: ";
+cin>>first_number >>second_number;
+
+int sum=first_number+second_number;
+cout<<first_number<<"+"<<second_number<<"="<<sum<<endl;
+
+
+
+    return 0;
+}
