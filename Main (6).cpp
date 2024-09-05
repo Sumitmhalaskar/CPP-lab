e0712131fe51f643f8e507bfd7419409f8225022
Sumit mhalaskar <sumit.mhalaskar24@sanjivani.edu.in>
2024-09-05 07:48:57 +0530

    Add files via upload
    C++ program to demonstrate syntax of switch

---
+++ Main (6).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    char x = 'A';
+    
+    
+    switch(x){
+      case'A':
+    cout<<"choose a A"<<endl;
+    break;
+    
+    case' b':
+    cout<<"choose a b"<<endl;
+    break;
+    case'c':
+    cout<<"choose a c"<<endl
+    break;
+    }
+    
+    return 0;
+}
