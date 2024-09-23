f5bd6731e1a9f3ef0e8aefe60c38dd185fd9bd7d
Sumit mhalaskar <sumit.mhalaskar24@sanjivani.edu.in>
2024-09-23 19:40:59 +0530

    Add files via upload
    This program will prompt the user to enter a number and then determine if the
    number is positive or negative.

---
+++ Main (20).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+int number;
+
+cout << "Enter a number: ";
+cin >> number;
+
+if (number >= 0) {
+cout << "The number " << number << " is positive.\n";
+} else {
+cout << "The number " << number << " is negative.\n";
+}
+return 0;
+}
